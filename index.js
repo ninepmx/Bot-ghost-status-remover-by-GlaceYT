8ed0212e7a9723a64a95348cd876bfaccefc5848
ninepmx <jamalbaouchi1906@gmail.com>
2024-09-29 09:40:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Spielt Rivaro | Roleplay"];
+const statusMessages = ["<:RivaroRoleplay:1269400142155612230> Schaut Rivaro | Roleplay zu"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
